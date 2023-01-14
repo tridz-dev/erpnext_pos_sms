26b503b1a3e21fba84c71f87b8c67375b05c3f98
Jabir Elat <88030376+jabir-tridz@users.noreply.github.com>
2023-01-14 07:05:56 +0530

    disable sms function

--- erpnext_pos_sms/sms.py
+++ erpnext_pos_sms/sms.py
@@ -8,14 +8,14 @@ from erpnext_pos_sms import url_shorten
 
 
 
-def send_sms(doc, event):
-    mobile_number = frappe.db.get_value('Customer', {'name': doc.customer}, ['mobile_number'])
-    from frappe.core.doctype.sms_settings.sms_settings import send_sms
-    #url = 
-    url = "http://store1.frapino.in/files/{}.pdf".format(doc.name)
-    url_short = url_shorten.make_shorten(url)
-    message = "Thank you for your purchase at Frapino. You have spent Rs " + str(doc.grand_total) +"via cash on "+ (doc.posting_date)+" "+"."+" Click the link to view your invoice "+ url_short+"."
-    send_sms([mobile_number], msg=message)
+# def send_sms(doc, event):
+#     mobile_number = frappe.db.get_value('Customer', {'name': doc.customer}, ['mobile_number'])
+#     from frappe.core.doctype.sms_settings.sms_settings import send_sms
+#     #url = 
+#     url = "http://store1.frapino.in/files/{}.pdf".format(doc.name)
+#     url_short = url_shorten.make_shorten(url)
+#     message = "Thank you for your purchase at Frapino. You have spent Rs " + str(doc.grand_total) +"via cash on "+ (doc.posting_date)+" "+"."+" Click the link to view your invoice "+ url_short+"."
+#     send_sms([mobile_number], msg=message)
     
 
     
